397f7b6cf8fd4c11a927db31aefe1e07065ccffd
asmi92 <asmitagosavi92@gmail.com>
2017-09-23 16:39:39 -0400

    updated index page

---
+++ SlackForODU/template/includes/functions.php
@@ -0,0 +1,39 @@
+<?php
+
+	function confirm_query($result_set) {
+		if (!$result_set) {
+			die("Database query failed.");
+		}
+	}
+
+	function redirect_to($new_location) {
+	  	header("Location: " . $new_location);
+	  	exit;
+	}
+
+	function verify_input($data) {
+		global $connection;
+  		$data = htmlspecialchars($data);
+  		$data = mysqli_real_escape_string($connection,$data);
+  		return $data;
+    }
+    function verify_in($data) {
+		global $connection;
+  		$data = mysqli_real_escape_string($connection,$data);
+  		return $data;
+    }
+	
+	function verify_output($data) {
+
+  		$data = stripcslashes($data);
+  		$data = htmlspecialchars_decode($data);
+  		return $data;
+    }
+    function verify_out($data) {
+
+  		$data = stripcslashes($data);
+  		return $data;
+    }
+
+//End of  Get Check Number
+?>
