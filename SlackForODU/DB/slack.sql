05856f394d450a36d60ef0a1fe08622538750f76
asmi92 <asmitagosavi92@gmail.com>
2017-09-30 17:41:54 -0400

    updated database file

--- SlackForODU/DB/slack.sql
+++ SlackForODU/DB/slack.sql
@@ -1,15 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 4.7.4
+-- version 4.6.5.2
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Sep 25, 2017 at 01:28 AM
--- Server version: 10.1.26-MariaDB
--- PHP Version: 7.1.9
+-- Generation Time: Sep 30, 2017 at 11:41 PM
+-- Server version: 10.1.21-MariaDB
+-- PHP Version: 5.6.30
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
-START TRANSACTION;
 SET time_zone = "+00:00";
 
 
@@ -29,35 +27,101 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `channel` (
-  `channelId` varchar(20) NOT NULL,
-  `channelName` varchar(60) NOT NULL,
-  `creator` varchar(20) NOT NULL,
-  `created` datetime NOT NULL
+  `channel_id` varchar(20) NOT NULL,
+  `channel_name` varchar(20) NOT NULL,
+  `channel_creator` varchar(20) NOT NULL,
+  `channel_created` datetime NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `channel`
+--
+
+INSERT INTO `channel` (`channel_id`, `channel_name`, `channel_creator`, `channel_created`) VALUES
+('ch1', 'general', 'mater', '2017-09-04 09:27:20');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `group`
+-- Table structure for table `login`
 --
 
-CREATE TABLE `group` (
+CREATE TABLE `login` (
+  `username` varchar(200) NOT NULL,
+  `password` varchar(200) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `login`
+--
+
+INSERT INTO `login` (`username`, `password`) VALUES
+('sampleuser1', 'samplepassword'),
+('sampleuser2', 'testing'),
+('testuser1', 'testpassword');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `slack_group`
+--
+
+CREATE TABLE `slack_group` (
   `group_id` varchar(20) NOT NULL,
-  `group_name` varchar(20) NOT NULL
+  `group_name` varchar(20) NOT NULL,
+  `group_creator` varchar(20) NOT NULL,
+  `group_created` datetime NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `user`
+-- Table structure for table `users`
 --
 
-CREATE TABLE `user` (
-  `userId` varchar(20) NOT NULL,
-  `name` varchar(70) NOT NULL,
+CREATE TABLE `users` (
+  `user_id` varchar(20) NOT NULL,
+  `username` varchar(20) NOT NULL,
   `password` varchar(20) NOT NULL,
-  `username` varchar(20) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 COMMENT='user data';
+  `email_id` varchar(30) NOT NULL,
+  `group_id` varchar(20) DEFAULT NULL,
+  `full_name` varchar(20) NOT NULL,
+  `workspace_id` varchar(20) NOT NULL,
+  `channel_id` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`user_id`, `username`, `password`, `email_id`, `group_id`, `full_name`, `workspace_id`, `channel_id`) VALUES
+('u1', 'mater', 'abc123', 'mater@rsprings.gov', NULL, 'Tow Mater', 'wk1', 'ch1'),
+('u2', 'sally', 'abcabc', 'porsche@rsprings.gov', NULL, 'Sally Carrera', 'wk1', 'ch1'),
+('u3', 'doc', 'abc123', 'hornet@rsprings.gov', 'g1', 'Doc Hudson', 'wk1', 'ch1'),
+('u4', 'mcmissile', 'abc123', 'topsecret@agent.org', NULL, 'Finn McMissile', 'wk2', 'ch1'),
+('u5', 'mcqueen', 'abc123', 'kachow@rusteze.com', NULL, 'Lightning McQueen', 'wk2', 'ch1');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `workspace`
+--
+
+CREATE TABLE `workspace` (
+  `wk_id` varchar(20) NOT NULL,
+  `url` varchar(50) NOT NULL,
+  `purpose` varchar(50) NOT NULL,
+  `wk_created` datetime NOT NULL,
+  `wk_creator` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `workspace`
+--
+
+INSERT INTO `workspace` (`wk_id`, `url`, `purpose`, `wk_created`, `wk_creator`) VALUES
+('wk1', 'odu-its-students', 'Education', '2017-09-01 05:35:00', 'mater'),
+('wk2', 'prasmik', 'Office', '2017-09-08 08:16:08', 'sally');
 
 --
 -- Indexes for dumped tables
@@ -67,14 +131,19 @@ CREATE TABLE `user` (
 -- Indexes for table `channel`
 --
 ALTER TABLE `channel`
-  ADD PRIMARY KEY (`channelId`);
+  ADD PRIMARY KEY (`channel_id`);
 
 --
--- Indexes for table `user`
+-- Indexes for table `login`
 --
-ALTER TABLE `user`
-  ADD PRIMARY KEY (`userId`);
-COMMIT;
+ALTER TABLE `login`
+  ADD PRIMARY KEY (`username`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`user_id`);
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
