23ac82876dd10f10e125c350925fba00b21c6077
Asmita Gosavi <asmitagosavi92@gmail.com>
2017-10-02 12:47:54 -0400

    Update member.php

--- SlackForODU/template/member.php
+++ SlackForODU/template/member.php
@@ -1,3 +1,6 @@
+<?php
+include 'includes/htmlheader.php';
+?>
 <?php 
 session_start();
 if(!isset($_SESSION["sess_user"])){
@@ -13,7 +16,7 @@ if(!isset($_SESSION["sess_user"])){
     </style>
 </head>
 <body>
-    <center><h1>Welcome to Slack For ODU</h1></center>
+    <center><h1>Welcome to Slack for ODU</h1></center>
     
 <h2>Welcome, <?=$_SESSION['sess_user'];?>! <a href="logout.php">Logout</a></h2>
 <p>
