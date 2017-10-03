3a1bc7b5627301fe919c6d37de8348045730a1f6
Asmita Gosavi <asmitagosavi92@gmail.com>
2017-10-03 14:31:19 -0400

    Update login.php

--- SlackForODU/template/login.php
+++ SlackForODU/template/login.php
@@ -7,64 +7,7 @@ include 'includes/htmlheader.php';
 <head>
 <title>Login</title>
     <style> 
-        body{
-            
-    margin-top: 100px;
-    margin-bottom: 100px;
-    margin-right: 150px;
-    margin-left: 80px;
-    background-color: azure ;
-    color: palevioletred;
-    font-family: verdana;
-    font-size: 100%;
-    background: #eee url("../images/background.png");      
-    
-        }
-            h1 {
-    color: indigo;
-    font-family: verdana;
-    font-size: 100%;
-}
-        h3 {
-    color: indigo;
-    font-family: verdana;
-    font-size: 100%;
-}
-  html,body{
-    position: relative;
-    height: 100%;
-}
-.login-container{
-    position: relative;
-    width: 300px;
-    margin: 80px auto;
-    padding: 20px 40px 40px;
-    text-align: center;
-    background: #fff;
-    border: 1px solid #ccc;
-}
-   
- 
-.login-container::before,.login-container::after{
-    content: "";
-    position: absolute;
-    width: 100%;height: 100%;
-    top: 3.5px;left: 0;
-    background: #fff;
-    z-index: -1;
-    -webkit-transform: rotateZ(4deg);
-    -moz-transform: rotateZ(4deg);
-    -ms-transform: rotateZ(4deg);
-    border: 1px solid #ccc;
-}
-.login-container::after{
-    top: 5px;
-    z-index: -2;
-    -webkit-transform: rotateZ(-2deg);
-     -moz-transform: rotateZ(-2deg);
-      -ms-transform: rotateZ(-2deg);
-}
-        
+     
     </style>
 </head>
 <body>
