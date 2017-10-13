edc13272200d284a6888aa7fb4fac7a5e74a7c43
Asmita Gosavi <asmitagosavi92@gmail.com>
2017-10-12 21:57:46 -0400

    Update milestone1dump.sql

--- SlackForODU/milestone1dump.sql
+++ SlackForODU/milestone1dump.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Sep 30, 2017 at 11:41 PM
+-- Generation Time: Oct 11, 2017 at 12:56 AM
 -- Server version: 10.1.21-MariaDB
 -- PHP Version: 5.6.30
 
@@ -41,6 +41,11 @@ CREATE TABLE `channel` (
 -- Dumping data for table `channel`
 --
 
+INSERT INTO `channel` (`channel_id`, `channel_name`, `channel_creator`, `channel_created`) VALUES
+('ch1', 'general', 'default', '2017-09-04 09:27:20'),
+('ch2', 'random', 'default', '2017-10-02 03:11:04'),
+('ch3', 'foodie', 'sally', '2017-10-01 07:15:00'),
+('ch4', 'travel', 'mater', '2017-09-04 05:15:26');
 
 -- --------------------------------------------------------
 
@@ -61,6 +66,49 @@ INSERT INTO `login` (`username`, `password`) VALUES
 ('sampleuser1', 'samplepassword'),
 ('sampleuser2', 'testing'),
 ('testuser1', 'testpassword');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `message`
+--
+
+CREATE TABLE `message` (
+  `msg_id` int(11) NOT NULL,
+  `subject` varchar(40) NOT NULL,
+  `creator_id` varchar(20) NOT NULL,
+  `msg_body` text NOT NULL,
+  `create_date` datetime NOT NULL,
+  `thread_id` int(11) NOT NULL,
+  `channel_id` varchar(20) DEFAULT NULL,
+  `group_id` varchar(20) NOT NULL,
+  `recipient_id` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `message`
+--
+
+INSERT INTO `message` (`msg_id`, `subject`, `creator_id`, `msg_body`, `create_date`, `thread_id`, `channel_id`, `group_id`, `recipient_id`) VALUES
+(5, 'private', 'mcqueen', 'gfhhhh', '2017-10-09 17:29:45', 0, 'ch1', 'g1', 'mcqueen'),
+(6, 'private', 'mcqueen', 'gfhhhh', '2017-10-09 17:31:17', 0, 'ch1', 'g1', 'mcqueen'),
+(7, 'private', 'mcqueen', 'gfhhhh', '2017-10-09 17:31:48', 0, 'ch1', 'g1', 'mcqueen'),
+(8, 'private', 'mcqueen', 'gfhhhh', '2017-10-09 17:32:13', 0, 'ch1', 'g1', 'mcqueen'),
+(9, 'private', 'mcqueen', 'gfhhhh', '2017-10-09 17:33:19', 0, 'ch1', 'g1', 'mcqueen'),
+(10, 'private', 'mcqueen', 'gfhhhh', '2017-10-09 17:33:59', 0, 'ch1', 'g1', 'mcqueen'),
+(11, 'private', 'mcqueen', 'gfhhhh', '2017-10-09 17:34:11', 0, 'ch1', 'g1', 'mcqueen'),
+(12, 'private', 'mcqueen', 'gfhhhh', '2017-10-09 17:34:12', 0, 'ch1', 'g1', 'mcqueen'),
+(13, 'private', 'mcqueen', '', '2017-10-10 17:12:28', 0, 'ch1', 'g1', 'mcqueen'),
+(14, 'private', 'mcqueen', '', '2017-10-10 17:12:36', 0, 'ch1', 'g1', 'mcqueen'),
+(15, 'private', 'mcqueen', '&lt;!dsjd&gt;', '2017-10-10 17:16:03', 0, 'ch1', 'g1', 'mcqueen'),
+(16, 'private', 'mcqueen', '&lt;!skdjksd&gt;', '2017-10-10 17:16:29', 0, 'ch1', 'g1', 'mcqueen'),
+(17, 'private', 'mcqueen', '&lt;!skdjksd&gt;', '2017-10-10 17:16:34', 0, 'ch1', 'g1', 'mcqueen'),
+(18, 'private', 'mcqueen', '&lt;!skdjksd&gt;', '2017-10-10 17:16:46', 0, 'ch1', 'g1', 'mcqueen'),
+(19, 'private', 'mcqueen', '&lt;!skdjksd&gt;', '2017-10-10 17:17:33', 0, 'ch1', 'g1', 'mcqueen'),
+(20, 'private', 'mcqueen', '&lt;!skdjksd&gt;', '2017-10-10 17:17:55', 0, 'ch1', 'g1', 'mcqueen'),
+(21, 'private', 'mcqueen', '&lt;!skdjksd&gt;', '2017-10-10 17:19:09', 0, 'ch1', 'g1', 'mcqueen'),
+(22, 'private', 'mcqueen', '&lt;djsjdkjskjkdjkdjckdcldlk&gt;', '2017-10-10 17:20:10', 0, 'ch1', 'g1', 'mcqueen'),
+(23, 'private', 'mcqueen', '&lt;djsjdkjskjkdjkdjckdcldlk&gt;', '2017-10-10 17:20:19', 0, 'ch1', 'g1', 'mcqueen');
 
 -- --------------------------------------------------------
 
@@ -89,19 +137,21 @@ CREATE TABLE `users` (
   `group_id` varchar(20) DEFAULT NULL,
   `full_name` varchar(20) NOT NULL,
   `workspace_id` varchar(20) NOT NULL,
-  `channel_id` varchar(20) NOT NULL
+  `channel_id` varchar(20) NOT NULL,
+  `profile_pic` varchar(50) NOT NULL,
+  `signup_date` datetime NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `users`
 --
 
-INSERT INTO `users` (`user_id`, `username`, `password`, `email_id`, `group_id`, `full_name`, `workspace_id`, `channel_id`) VALUES
-('u1', 'mater', 'abc123', 'mater@rsprings.gov', NULL, 'Tow Mater', 'wk1', 'ch1'),
-('u2', 'sally', 'abcabc', 'porsche@rsprings.gov', NULL, 'Sally Carrera', 'wk1', 'ch1'),
-('u3', 'doc', 'abc123', 'hornet@rsprings.gov', 'g1', 'Doc Hudson', 'wk1', 'ch1'),
-('u4', 'mcmissile', 'abc123', 'topsecret@agent.org', NULL, 'Finn McMissile', 'wk2', 'ch1'),
-('u5', 'mcqueen', 'abc123', 'kachow@rusteze.com', NULL, 'Lightning McQueen', 'wk2', 'ch1');
+INSERT INTO `users` (`user_id`, `username`, `password`, `email_id`, `group_id`, `full_name`, `workspace_id`, `channel_id`, `profile_pic`, `signup_date`) VALUES
+('u1', 'mater', 'abc123', 'mater@rsprings.gov', NULL, 'Tow Mater', 'wk1', 'ch1', '1.png', '0000-00-00 00:00:00'),
+('u2', 'sally', 'abcabc', 'porsche@rsprings.gov', NULL, 'Sally Carrera', 'wk1', 'ch1', '2.png', '2017-09-01 03:00:00'),
+('u3', 'doc', 'abc123', 'hornet@rsprings.gov', 'g1', 'Doc Hudson', 'wk1', 'ch1', '3.png', '2017-09-04 09:00:00'),
+('u4', 'mcmissile', 'abc123', 'topsecret@agent.org', NULL, 'Finn McMissile', 'wk2', 'ch1', '6.png', '2017-09-06 05:15:00'),
+('u5', 'mcqueen', 'abc123', 'kachow@rusteze.com', NULL, 'Lightning McQueen', 'wk2', 'ch1', '7.png', '2017-09-05 10:08:00');
 
 -- --------------------------------------------------------
 
@@ -114,16 +164,17 @@ CREATE TABLE `workspace` (
   `url` varchar(50) NOT NULL,
   `purpose` varchar(50) NOT NULL,
   `wk_created` datetime NOT NULL,
-  `wk_creator` varchar(20) NOT NULL
+  `wk_creator` varchar(20) NOT NULL,
+  `channel_id` varchar(20) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `workspace`
 --
 
-INSERT INTO `workspace` (`wk_id`, `url`, `purpose`, `wk_created`, `wk_creator`) VALUES
-('wk1', 'odu-its-students', 'Education', '2017-09-01 05:35:00', 'mater'),
-('wk2', 'prasmik', 'Office', '2017-09-08 08:16:08', 'sally');
+INSERT INTO `workspace` (`wk_id`, `url`, `purpose`, `wk_created`, `wk_creator`, `channel_id`) VALUES
+('wk1', 'odu-its-students', 'Education', '2017-09-01 05:35:00', 'mater', 'ch1,ch2,ch3'),
+('wk2', 'prasmik', 'Office', '2017-09-08 08:16:08', 'sally', 'ch1,ch2,ch3,ch4');
 
 --
 -- Indexes for dumped tables
@@ -142,16 +193,32 @@ ALTER TABLE `login`
   ADD PRIMARY KEY (`username`);
 
 --
+-- Indexes for table `message`
+--
+ALTER TABLE `message`
+  ADD PRIMARY KEY (`msg_id`);
+
+--
 -- Indexes for table `users`
 --
 ALTER TABLE `users`
   ADD PRIMARY KEY (`user_id`);
 
-INSERT INTO `channel` (`channel_id`, `channel_name`, `channel_creator`, `channel_created`) VALUES
-('ch1', 'general', 'default', '2017-09-04 09:27:20'),
-('ch2', 'random', 'default', '2017-10-02 03:11:04'),
-('ch3', 'foodie', 'sally', '2017-10-01 07:15:00'),
-('ch4', 'travel', 'mater', '2017-09-04 05:15:26');
+--
+-- Indexes for table `workspace`
+--
+ALTER TABLE `workspace`
+  ADD PRIMARY KEY (`wk_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `message`
+--
+ALTER TABLE `message`
+  MODIFY `msg_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=24;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
