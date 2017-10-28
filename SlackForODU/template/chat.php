90c9949cdbf47017c31bd1ee60dd10416c622d9c
asmi92 <asmitagosavi92@gmail.com>
2017-10-28 19:45:34 -0400

    conflict resolving

--- SlackForODU/template/chat.php
+++ SlackForODU/template/chat.php
@@ -114,14 +114,9 @@
                         }
                         ?></center>
                     <div class="chat-message clearfix">
+                        <img src="../images/<?php echo $value['profile_pic'] ?>" alt="profile pic" width="24" height="24">
 
-<<<<<<< HEAD
-<!--                         <img src="../images/<?php echo $value['profile_pic'] ?>" alt="profile pic" width="24" height="24">
- --> 
-=======
-<!--                         <img src="../images/<?php echo $value['profile_pic'] ?>" alt="profile pic" width="24" height="24"> -->
- 
->>>>>>> 37e5002df73d9ff1c376340b420c2ac2630283b4
+
                         <div class="chat-message-content clearfix">
 
                             <span class="chat-time"><?php echo $crdate ?></span>
