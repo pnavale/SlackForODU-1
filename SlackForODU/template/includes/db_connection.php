fd204ef5ce5c52e19cac4b26708fa5d1d51210e7
Asmita Gosavi <asmitagosavi92@gmail.com>
2017-10-28 13:07:13 -0400

    Update db_connection.php

--- SlackForODU/template/includes/db_connection.php
+++ SlackForODU/template/includes/db_connection.php
@@ -10,8 +10,8 @@
 
 // Create connection
 
-$connection=mysqli_connect('localhost','root','');
-//$connection=mysqli_connect('localhost','admin','M0n@rch$');
+//$connection=mysqli_connect('localhost','root','');
+$connection=mysqli_connect('localhost','admin','M0n@rch$');
 //mysql_select_db('slack',$connection)or die(mysql_error());
 mysqli_select_db($connection,'slack')or die(mysqli_error($connection));
 
