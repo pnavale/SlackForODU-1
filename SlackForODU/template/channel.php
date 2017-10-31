838a820cc862b12870787ebd8b6ddb96aa79edda
asmi92 <asmitagosavi92@gmail.com>
2017-10-31 15:13:56 -0400

    channel validations

--- SlackForODU/template/channel.php
+++ SlackForODU/template/channel.php
@@ -10,60 +10,58 @@ if (!isset($_SESSION["sess_user"])) {
   <?php
 // define variables and set to empty values
 $nameErr = $purposeErr = $invitesErr = "";
-$name = $purpose = $c = $invites = $chType = $channel_id =$uninvited= "";
+$name = $purpose = $c = $invites = $chType = $channel_id = $uninvited = "";
 $members = [];
 if (isset($_POST["submit"])) {
     $name = verify_input($_POST['name']);
     if (!empty($name)) {
         $lowercaseName = strtolower($name);
-        if (($name != $lowercaseName) || ctype_space($name) || strpos($name, '.')) {
+        if (($name != $lowercaseName) || ctype_space($name) || strpos($name, '.') || strpos($name, ' ')) {
             $error = "Please enter channel name in lowercase without spaces or period.";
-        } else {
+        } else if (($name == $lowercaseName) && !ctype_space($name) && !strpos($name, '.') && !strpos($name, ' ')) {
             if (isset($_POST['purpose'])) {
                 $purpose = verify_input($_POST['purpose']);
             }
-            
+
             if (isset($_POST['chType'])) {
                 $chType = $_POST['chType'];
             } else {
                 $chType = "public";
             }
             $query = "SELECT * FROM users where workspace_id='" . $_SESSION['wkid'] . "'";
-    $result = $connection->query($query);
-    if ($result->num_rows > 0) {
-        while ($row = $result->fetch_assoc()) {
-            array_push($members, $row);
-        }
-    }
-             foreach ($members as $member) { 
-                    $uninvited=$uninvited . "," . $member['username'];
-                   }
+            $result = $connection->query($query);
+            if ($result->num_rows > 0) {
+                while ($row = $result->fetch_assoc()) {
+                    array_push($members, $row);
+                }
+            }
+            foreach ($members as $member) {
+                $uninvited = $uninvited . "," . $member['username'];
+            }
             if (isset($_POST['invites'])) {
                 foreach ($_POST['invites'] as $selectedOption) {
                     $invites = $invites . "," . $selectedOption;
                 }
-                
-            }else{
-                if($chType == "public"){
+            } else {
+                if ("public" == $chType) {
                     $query = "SELECT * FROM users where workspace_id='" . $_SESSION['wkid'] . "'";
                     $result = $connection->query($query);
                     if ($result->num_rows > 0) {
                         while ($row = $result->fetch_assoc()) {
-                            $invites = $invites . "," .$row['username'];
+                            $invites = $invites . "," . $row['username'];
                         }
                     }
                 }
             }
-        $uninvited=str_replace($invites,'',$uninvited); 
-                    
-            
-        }
+            $uninvited = str_replace($invites, '', $uninvited);
+
             $query = "SELECT * FROM channel where channel_name='" . $name . "'";
             $result = $connection->query($query);
-            $user=$_SESSION['sess_user'];
+            $user = $_SESSION['sess_user'];
+            $wk_id = $_SESSION['wkid'];
             if ($result->num_rows > 0) {
                 while ($row = $result->fetch_assoc()) {
-                    $error = "This channel already exists. Please use different name to start a new channel or go to dashboard to see this channel.";
+                    $error = 'This channel already exists. Please use different name to start a new channel or go to dashboard to see this channel.';
                 }
             } else {
                 $result1 = $connection->query("insert into channel (channel_name,channel_creator,channel_created,wk_id,channel_type,purpose,invites,uninvited)    values('$name','$user',NOW(),'$wk_id','$chType','$purpose','$invites','$uninvited');
@@ -74,7 +72,7 @@ if (isset($_POST["submit"])) {
                     echo mysqli_error($connection);
                 }
             }
-        
+        }
     } else {
         $error = 'Please enter the channel name.';
     }
