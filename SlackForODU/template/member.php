a0155108ac19e06f9176e563688ccf3e892773b8
Pratik Navale <31637664+pnavale@users.noreply.github.com>
2017-11-12 19:34:01 -0500

    Added details

--- SlackForODU/template/member.php
+++ SlackForODU/template/member.php
@@ -1,5 +1,6 @@
 <?php
 session_start();
+//Go to Login page if not already logged in.
 if (!isset($_SESSION["sess_user"])) {
     header("location:login.php");
 }
@@ -8,11 +9,14 @@ include 'includes/htmlheader.php';
     <center>
         <h1>Welcome to Slack for ODU</h1>
     </center>
+    <!-- Display username. -->
     <h3>Welcome, <?=$_SESSION['sess_user_fullname'];?>! <a href="logout.php" class="pull-right">Logout</a></h3>
     <div class="row">
+        <!-- Display sidebar. -->
         <div class="col-sm-0 col-md-3 col-lg-3 col-xs-0" style="background-color:#2c2d30;">
             <?php include 'sidebar.php';?>
         </div>
+        <!-- Display chat pane. -->
         <div class="col-sm-12 col-md-9 col-lg-9 col-xs-12">
             <?php include 'chat.php';?>
         </div>
