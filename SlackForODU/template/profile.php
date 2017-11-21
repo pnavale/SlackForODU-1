555bfe75425f9e089bf30d79c3cbe3c0d80c07a7
asmi92 <asmitagosavi92@gmail.com>
2017-11-21 17:37:05 -0500

    posts

--- SlackForODU/template/profile.php
+++ SlackForODU/template/profile.php
@@ -62,7 +62,7 @@ if (!$_SESSION['wkid']) {
 <a class="nounderline" title="Posts">
 <div class="statBubbleContainer col-sm-2 col-md-4 col-lg-4 col-xs-2">
 <div class="statBubble topSEORank">
-  <div class="statNum" id="member-metric">
+  <div class="statNum" id="posts-metric">
   0
   </div>
 </div>
@@ -162,6 +162,7 @@ if (!$_SESSION['wkid']) {
                 channelDiv= $('.channel-list');
                 $('#channel-metric').html(response['channels'].length);
                 $('#member-metric').html(response['users'].length);
+                $('#posts-metric').html(response['posts'].length);
                 $('#reaction-metric').html(response['reactions'].length);
                 $.each( response['channels'], function( key, channel ) {
                         if(channel['channel_type']=='public'){
