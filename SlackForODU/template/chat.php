30fc61771a915e1136fab90aca73382727cf0f87
asmi92 <asmitagosavi92@gmail.com>
2017-11-28 13:01:28 -0500

    image preview

--- SlackForODU/template/chat.php
+++ SlackForODU/template/chat.php
@@ -266,11 +266,12 @@ if( $page > 0 && $left_rec > $limit) {
       <form action="" enctype="multipart/form-data" method="POST" >
                 <br> Upload your image here:
                 <input type="hidden" name="MAX_FILE_SIZE" value="10000000" />
-                <input name="userfile1" type="file" />
+                <input name="userfile1" type="file"  id="file-input" />
                 <br>
                 <br>
                 <input type="submit" class="btn btn-success" value="Next" name="img" style="width:30%;" /><br>
                 <input type="button" class="btn btn-default" value="Cancel" style="width:30%;" onclick="off()" />
+                <div id="thumb-output"></div>
     </form>
   </div>
   <div id="tabs-2">
@@ -285,12 +286,40 @@ if( $page > 0 && $left_rec > $limit) {
     <input type="submit" class="btn btn-success" value="Next" name="webimg" id="web-img" style="width:30%;" /><br>
     <input type="button" class="btn btn-default" value="Cancel" style="width:30%;" onclick="off()" />
     </form>
+      <img src="http://webspace.webring.com/people/jv/vladilyich/preview.gif" class="preview" />
   </div>
 </div>
 </div>
 </div>
 
-
+<script>
+$('#file-input').on('change', function(){ //on file input change
+        if (window.File && window.FileReader && window.FileList && window.Blob) //check File API supported browser
+        {
+            $('#thumb-output').html(''); //clear html of output element
+            var data = $(this)[0].files; //this file data
+            
+            $.each(data, function(index, file){ //loop though each file
+                if(/(\.|\/)(gif|jpe?g|png)$/i.test(file.type)){ //check supported file type
+                    var fRead = new FileReader(); //new filereader
+                    fRead.onload = (function(file){ //trigger function on successful read
+                    return function(e) {
+                        var img = $('<img/>').addClass('thumb').attr('src', e.target.result); //create image element 
+                        $('#thumb-output').append(img); //append image to output element
+                    };
+                    })(file);
+                    fRead.readAsDataURL(file); //URL representing the file's data.
+                }
+            });
+            
+        }else{
+            alert("Your browser doesn't support File API!"); //if File API is absent
+        }
+    });
+$('[name="webupload"]').on('change', function() {
+     $('img.preview').prop('src', this.value);
+});
+</script>
 
 
 <?php
