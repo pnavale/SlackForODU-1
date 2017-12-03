ea138907006db818a7e61fe6294c797eb2b68f6b
asmi92 <asmitagosavi92@gmail.com>
2017-12-03 17:08:08 -0500

    after conflict

--- SlackForODU/template/init.php
+++ SlackForODU/template/init.php
@@ -5,7 +5,7 @@ function goToAuthUrl(){
 	$redirect_url = 'http://asmi92.cs518.cs.odu.edu/SlackForODU/template/callback.php';
 	if($_SERVER['REQUEST_METHOD'] == 'GET'){
 		$url = 'https://github.com/login/oauth/authorize?client_id='.$client_id.'&redirect_url='.$redirect_url.'&scope=user'; 
-		header('location: $url');
+		header('location:'. $url);
 	}
 }
 ?>
