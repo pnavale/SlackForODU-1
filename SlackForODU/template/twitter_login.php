8f6c5b2e03963d94a5e38032684a36c1d760f09d
asmi92 <asmitagosavi92@gmail.com>
2017-12-04 18:21:41 -0500

    twitter integration

--- SlackForODU/template/twitter_login.php
+++ SlackForODU/template/twitter_login.php
@@ -1,60 +1,35 @@
 <?php 
-include_once("twitter/twitteroauth.php");
-define('CONSUMER_KEY', '3R3owmBJlwURYQci6SConhxmK'); // YOUR CONSUMER KEY
-define('CONSUMER_SECRET', 'j2hZy9AuC1GYEImQEPx13wUas2VOnMMemIYkvtPflM6TaLSrCd'); //YOUR CONSUMER SECRET KEY 
-define('OAUTH_CALLBACK', 'http://asmi92.cs518.cs.odu.edu/SlackForODU/template/twitter_login.php');  // Redirect URL 
-if(isset($_GET['request']))
-{
-		//Fresh authentication
-		$connection = new TwitterOAuth(CONSUMER_KEY, CONSUMER_SECRET);
-		$request_token = $connection->getRequestToken(OAUTH_CALLBACK);
-
-		//Received token info from twitter
-		$_SESSION['token'] 			= $request_token['oauth_token'];
-		$_SESSION['token_secret'] 	= $request_token['oauth_token_secret'];
-
-		//Any value other than 200 is failure, so continue only if http code is 200
-		if($connection->http_code == '200')
-		{
-		//redirect user to twitter
-		$twitter_url = $connection->getAuthorizeURL($request_token['oauth_token']);
-		header('Location: ' . $twitter_url); 
-		}else{
-		die("error connecting to twitter! try again later!");
-		}
+session_start();
+//add autoload note:do check your file paths in autoload.php
+require "autoload.php";
+use Abraham\TwitterOAuth\TwitterOAuth;
+//this code will run when returned from twiter after authentication
+if(isset($_SESSION['oauth_token'])){
+  $oauth_token=$_SESSION['oauth_token'];unset($_SESSION['oauth_token']);
+  $consumer_key = '3R3owmBJlwURYQci6SConhxmK';
+  $consumer_secret = 'j2hZy9AuC1GYEImQEPx13wUas2VOnMMemIYkvtPflM6TaLSrCd';
+  $connection = new TwitterOAuth($consumer_key, $consumer_secret);
+ //necessary to get access token other wise u will not have permision to get user info
+  $params=array("oauth_verifier" => $_GET['oauth_verifier'],"oauth_token"=>$_GET['oauth_token']);
+  $access_token = $connection->oauth("oauth/access_token", $params);
+  //now again create new instance using updated return oauth_token and oauth_token_secret because old one expired if u dont u this u will also get token expired error
+  $connection = new TwitterOAuth($consumer_key, $consumer_secret,
+  $access_token['oauth_token'],$access_token['oauth_token_secret']);
+  $content = $connection->get("account/verify_credentials");
+  print_r($content);
 }
+else{
+  // main startup code
+  $consumer_key = '3R3owmBJlwURYQci6SConhxmK';
+  $consumer_secret = 'j2hZy9AuC1GYEImQEPx13wUas2VOnMMemIYkvtPflM6TaLSrCd';
+  //this code will return your valid url which u can use in iframe src to popup or can directly view the page as its happening in this example
 
-
-?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-	<title>Login with Twitter</title>
-
-</head>
-<body>
-<?php
-	if(isset($_REQUEST['oauth_token']) && $_SESSION['token'] == $_REQUEST['oauth_token']){
-
-			$connection = new TwitterOAuth(CONSUMER_KEY, CONSUMER_SECRET, $_SESSION['token'] , $_SESSION['token_secret']);
-			$access_token = $connection->getAccessToken($_REQUEST['oauth_verifier']);
-			if($connection->http_code == '200')
-			{
-
-				$user_data = $connection->get('account/verify_credentials'); 
-				$result = '<h1>Twiiter Profile Details </h1>';
-				$result .= '<img src="'.$user_data['profile_image_url'].'">';
-				$result .= '<br/>Twiiter ID : ' . $user_data['id'];
-				$result .= '<br/>Name : ' . $user_data['name'];
-				$result .= '<br/>Twiiter Handle : ' . $user_data['screen_name'];
-				$result .= '<br/>Follower : ' . $user_data['followers_count'];
-				$result .= '<br/>Follows : ' . $user_data['friends_count'];
-				$result .= '<br/>Logout from <a href="logout.php?logout">Twiiter</a>';
-                echo '<div>'.$result.'</div>';				
-			}else{
-			       die("error, try again later!");
-			}
-			
-	}
+  $connection = new TwitterOAuth($consumer_key, $consumer_secret);
+  $temporary_credentials = $connection->oauth('oauth/request_token', array("oauth_callback" =>'http://asmi92.cs518.cs.odu.edu/SlackForODU/template/twitter_login.php'));
+  $_SESSION['oauth_token']=$temporary_credentials['oauth_token'];       
+  $_SESSION['oauth_token_secret']=$temporary_credentials['oauth_token_secret'];
+  $url = $connection->url("oauth/authorize", array("oauth_token" => $temporary_credentials['oauth_token']));
+// REDIRECTING TO THE URL
+  header('Location: ' . $url); 
+}
 ?>
