db357606015b60e2fd8d22e16e34074c6d3c3927
asmi92 <asmitagosavi92@gmail.com>
2017-12-04 21:19:35 -0500

    twitter

--- SlackForODU/template/twitter_login.php
+++ SlackForODU/template/twitter_login.php
@@ -16,7 +16,7 @@ if (!isset($_SESSION['access_token'])) {
   $access_token = $_SESSION['access_token'];
   $connection = new TwitterOAuth(CONSUMER_KEY, CONSUMER_SECRET, $access_token['oauth_token'], $access_token['oauth_token_secret']);
   $params = array('include_email' => 'true', 'include_entities' => 'false', 'skip_status' => 'true');
-  $user = $connection->get("account/verify_credentials");
+  $user = $connection->get("account/verify_credentials", $params);
   echo $user->screen_name;
   echo $user->profile_image_url;
   echo $user->id;
