ee633ca1e827992c592aae51b2630bf6f701d22e
asmi92 <asmitagosavi92@gmail.com>
2017-12-04 21:28:16 -0500

    twitter

--- SlackForODU/template/twitter_login.php
+++ SlackForODU/template/twitter_login.php
@@ -11,7 +11,7 @@ if (!isset($_SESSION['access_token'])) {
   $_SESSION['oauth_token'] = $request_token['oauth_token'];
   $_SESSION['oauth_token_secret'] = $request_token['oauth_token_secret'];
   $url = $connection->url('oauth/authorize', array('oauth_token' => $request_token['oauth_token']));
-  redirect_to($url);
+  header("Location: ".$url);
 } else {
   $access_token = $_SESSION['access_token'];
   $connection = new TwitterOAuth(CONSUMER_KEY, CONSUMER_SECRET, $access_token['oauth_token'], $access_token['oauth_token_secret']);
