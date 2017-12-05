fef1cf93f58a2be5a27fd28899d59df89af200ac
asmi92 <asmitagosavi92@gmail.com>
2017-12-04 23:20:24 -0500

    twitter

--- SlackForODU/template/twitter_login.php
+++ SlackForODU/template/twitter_login.php
@@ -32,7 +32,7 @@ if (!isset($_SESSION['access_token'])) {
   // $_SESSION['sess_user_fullname'] = $row["full_name"];
   //   //https://twitter.com/TwitterEng/profile_image?size=original 
   // $_SESSION['sess_user_profile_pic'] = 'https://twitter.com/'.$row["username"].'profile_image?size=original';
-  header("Location: member.php");
+  // header("Location: member.php");
   // $query = "SELECT * from users where email_id = '".$email_id."'";
   // echo $query;
   // $result = $connection->query($query);
