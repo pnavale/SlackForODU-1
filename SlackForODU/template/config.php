7e276f95690faee4691ad4efeb2ae834909399aa
asmi92 <asmitagosavi92@gmail.com>
2017-12-04 19:19:09 -0500

    twitter

---
+++ SlackForODU/template/config.php
@@ -0,0 +1,5 @@
+<?php
+define('CONSUMER_KEY', '3R3owmBJlwURYQci6SConhxmK'); // YOUR CONSUMER KEY
+define('CONSUMER_SECRET', 'j2hZy9AuC1GYEImQEPx13wUas2VOnMMemIYkvtPflM6TaLSrCd'); //YOUR CONSUMER SECRET KEY 
+define('OAUTH_CALLBACK', 'http://asmi92.cs518.cs.odu.edu/SlackForODU/template/twitter_login.php');  // Redirect URL 
+?>
