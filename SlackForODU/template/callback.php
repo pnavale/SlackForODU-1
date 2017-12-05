531a085332dec27d3eff22504a3dd3d22c8df9db
asmi92 <asmitagosavi92@gmail.com>
2017-12-04 20:57:02 -0500

    twitter

--- SlackForODU/template/callback.php
+++ SlackForODU/template/callback.php
@@ -1,58 +1,14 @@
 <?php
-define('CONSUMER_KEY', 'YOUR CONSUMER KEY'); // YOUR CONSUMER KEY
-define('CONSUMER_SECRET', 'YOUR CONSUMER SECRET KEY'); //YOUR CONSUMER SECRET KEY 
-define('OAUTH_CALLBACK', 'http://localhost/login_with_twitter_using_php/index.php');  // Redirect URL 
-require_once 'vendor/autoload.php';
-use Abraham\TwitterOAuth\TwitterOAuth;
 session_start();
-$config = $config =[
-    'consumer_key'      => '3R3owmBJlwURYQci6SConhxmK',
-    'consumer_secret'   => 'j2hZy9AuC1GYEImQEPx13wUas2VOnMMemIYkvtPflM6TaLSrCd',
-    'url_login'         => 'http://asmi92.cs518.cs.odu.edu/SlackForODU/',
-    'url_callback'      => 'http://asmi92.cs518.cs.odu.edu/SlackForODU/template/callback.php',
-];
-// get and filter oauth verifier
-$oauth_verifier = filter_input(INPUT_GET, 'oauth_verifier');
-// check tokens
-if (empty($oauth_verifier) ||
-    empty($_SESSION['oauth_token']) ||
-    empty($_SESSION['oauth_token_secret'])
-) {
-    // something's missing, go and login again
-    header('Location: ' . $config['url_login']);
+require 'autoload.php';
+use Abraham\TwitterOAuth\TwitterOAuth;
+if (isset($_REQUEST['oauth_verifier'], $_REQUEST['oauth_token']) && $_REQUEST['oauth_token'] == $_SESSION['oauth_token']) {
+    $request_token = [];
+    $request_token['oauth_token'] = $_SESSION['oauth_token'];
+    $request_token['oauth_token_secret'] = $_SESSION['oauth_token_secret'];
+    $connection = new TwitterOAuth(CONSUMER_KEY, CONSUMER_SECRET, $request_token['oauth_token'], $request_token['oauth_token_secret']);
+    $access_token = $connection->oauth("oauth/access_token", array("oauth_verifier" => $_REQUEST['oauth_verifier']));
+    $_SESSION['access_token'] = $access_token;
+    // redirect user back to index page
+    header('Location: ./');
 }
-// connect with application token
-$connection = new TwitterOAuth(
-    $config['consumer_key'],
-    $config['consumer_secret'],
-    $_SESSION['oauth_token'],
-    $_SESSION['oauth_token_secret']
-);
-// request user token
-$token = $connection->oauth(
-    'oauth/access_token', [
-        'oauth_verifier' => $oauth_verifier
-    ]
-);
-// connect with user token
-$twitter = new TwitterOAuth(
-    $config['consumer_key'],
-    $config['consumer_secret'],
-    $token['oauth_token'],
-    $token['oauth_token_secret']
-);
-$user = $twitter->get('account/verify_credentials');
-// if something's wrong, go and log in again
-if(isset($user->error)) {
-    header('Location: ' . $config['url_login']);
-}else{
-    header('Location: ' . member.php);
-}
-// post a tweet
-$status = $twitter->post(
-    "statuses/update", [
-        "status" => "You joined SlackForODU"
-    ]
-);
-echo ('Created new status with #' . $status->id . PHP_EOL);
-print_r($status);
--- SlackForODU/template/twitter_login.php
+++ SlackForODU/template/twitter_login.php
@@ -1,70 +1,20 @@
 <?php
-
-
-// Include config file and twitter PHP Library
-include_once("config.php");
-include_once("twitter/twitteroauth.php");
-echo $_GET['request'];
-if(isset($_GET['request']))
-{
-    //Fresh authentication
-    $connection = new TwitterOAuth(CONSUMER_KEY, CONSUMER_SECRET);
-    $request_token = $connection->getRequestToken(OAUTH_CALLBACK);
-
-    //Received token info from twitter
-    $_SESSION['token']      = $request_token['oauth_token'];
-    $_SESSION['token_secret']   = $request_token['oauth_token_secret'];
-    echo $_SESSION['token'];
-    //Any value other than 200 is failure, so continue only if http code is 200
-    if($connection->http_code == '200')
-    {
-    echo "inside if";
-    //redirect user to twitter
-    $twitter_url = $connection->getAuthorizeURL($request_token['oauth_token']);
-    echo "inside if".$twitter_url;
-    header('Location: ' . $twitter_url); 
-    }else{
-    die("error connecting to twitter! try again later!");
-    }
+session_start();
+require 'autoload.php';
+use Abraham\TwitterOAuth\TwitterOAuth;
+define('CONSUMER_KEY', '3R3owmBJlwURYQci6SConhxmK'); // add your app consumer key between single quotes
+define('CONSUMER_SECRET', 'j2hZy9AuC1GYEImQEPx13wUas2VOnMMemIYkvtPflM6TaLSrCd'); // add your app consumer secret key between single quotes
+define('OAUTH_CALLBACK', 'http://asmi92.cs518.cs.odu.edu/SlackForODU/template/twitter_login.php'); // your app callback URL
+if (!isset($_SESSION['access_token'])) {
+  $connection = new TwitterOAuth(CONSUMER_KEY, CONSUMER_SECRET);
+  $request_token = $connection->oauth('oauth/request_token', array('oauth_callback' => OAUTH_CALLBACK));
+  $_SESSION['oauth_token'] = $request_token['oauth_token'];
+  $_SESSION['oauth_token_secret'] = $request_token['oauth_token_secret'];
+  $url = $connection->url('oauth/authorize', array('oauth_token' => $request_token['oauth_token']));
+  echo $url;
+} else {
+  $access_token = $_SESSION['access_token'];
+  $connection = new TwitterOAuth(CONSUMER_KEY, CONSUMER_SECRET, $access_token['oauth_token'], $access_token['oauth_token_secret']);
+  $user = $connection->get("account/verify_credentials");
+  echo $user->screen_name;
 }
-
-
-?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-  <title>Login with Twitter</title>
-
-</head>
-<body>
-<?php
-  if(isset($_REQUEST['oauth_token']) && $_SESSION['token'] == $_REQUEST['oauth_token']){
-
-      $connection = new TwitterOAuth(CONSUMER_KEY, CONSUMER_SECRET, $_SESSION['token'] , $_SESSION['token_secret']);
-      $access_token = $connection->getAccessToken($_REQUEST['oauth_verifier']);
-      if($connection->http_code == '200')
-      {
-
-        $user_data = $connection->get('account/verify_credentials'); 
-        $result = '<h1>Twiiter Profile Details </h1>';
-        $result .= '<img src="'.$user_data['profile_image_url'].'">';
-        $result .= '<br/>Twiiter ID : ' . $user_data['id'];
-        $result .= '<br/>Name : ' . $user_data['name'];
-        $result .= '<br/>Twiiter Handle : ' . $user_data['screen_name'];
-        $result .= '<br/>Follower : ' . $user_data['followers_count'];
-        $result .= '<br/>Follows : ' . $user_data['friends_count'];
-        $result .= '<br/>Logout from <a href="logout.php?logout">Twiiter</a>';
-                echo '<div>'.$result.'</div>';        
-      }else{
-             die("error, try again later!");
-      }
-      
-  }else{
-    //Display login button
-    echo '<a href="twitter_login.php?request=twitter"><img src="../images/login_button.jpg" /></a>';
-  }
-?>  
-
-</body>
-</html>
