d8781810213a228b62fd99780fa171668361982a
asmi92 <asmitagosavi92@gmail.com>
2017-12-04 19:36:37 -0500

    twitter

--- SlackForODU/template/twitter_login.php
+++ SlackForODU/template/twitter_login.php
@@ -58,6 +58,9 @@ if(isset($_GET['request']))
              die("error, try again later!");
       }
       
+  }else{
+    //Display login button
+    echo '<a href="twitter_login.php?request=twitter"><img src="image/login_button.jpg" /></a>';
   }
 ?>  
 
