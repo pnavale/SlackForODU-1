700d1b11439143c409dd68423cc68b8d9cbf9a01
asmi92 <asmitagosavi92@gmail.com>
2017-12-06 15:02:00 -0500

    file upload

--- SlackForODU/template/chat.php
+++ SlackForODU/template/chat.php
@@ -136,7 +136,7 @@ if ($result->num_rows > 0) {
         //echo "<p>Uploaded file type:".$value['file_type']."</p>";
         //echo "<p>Uploaded file size:".$value['file_size']."</p>";
         //echo "<a href='".$value['file']."' target='_blank'>view file</a></div>";
-        echo "<a href='uploads/".$value['msg_body']."' target='_blank'>view file</a></div>";
+        echo "<a href='uploads/".base64_encode($value['file'])."' target='_blank'>view file</a></div>";
       }
 
      ?>
