4a841093d2303ee55520572b9565be5d10b08f63
asmi92 <asmitagosavi92@gmail.com>
2017-12-10 15:37:04 -0500

    twitter

--- SlackForODU/template/sideBarData.php
+++ SlackForODU/template/sideBarData.php
@@ -11,13 +11,38 @@ $channels = [];
 $uninvited = [];
 $members = [];
 $uninvitedStr = $invitesStr = '';
-if($_SESSION["twitter_user"] || $_SESSION["git_user"]){
+if($_SESSION["twitter_user"]){
     $query = "SELECT * FROM users where email_id='".$_SESSION['email_id']."'";
     $result = $connection->query($query);
-    if ($result->num_rows > 0) {
+    if ($result->num_rows > 0){
         while ($row = $result->fetch_assoc()) {
-            $_SESSION['sess_user']=$row['username'];
+            $_SESSION["username"] = $row["username"];
+            $_SESSION["twitter_user"] = 'True';
+            $_SESSION['sess_user_fullname'] = $row["full_name"];
+            //https://twitter.com/TwitterEng/profile_image?size=original 
+            $_SESSION['sess_user_profile_pic'] = 'https://twitter.com/'.$row["username"].'profile_image?size=original';
+              }
+    }else {
+        // echo "User do not Exists";
+        $query  = "INSERT INTO users(username, full_name, email_id, signup_date,group_id, workspace_id, profile_pic) VALUES ('{$_SESSION['username']}', '{$_SESSION['sess_user_fullname']}','{$_SESSION['email_id']}', NOW(),'twitteruser','{$_SESSION['wkid']}','{$_SESSION['sess_user_profile_pic']}' ) ";
+        $result_id = mysqli_query($connection, $query);
+        $result2 = $connection->query("SELECT * FROM channel");
+        if ($result2->num_rows > 0) {
+            while ($row = $result2->fetch_assoc()) {
+                $uninvited = $row['uninvited'] . "," . $username;  
+                if($row['channel_name']!='general' && $row['channel_name']!='random'){  
+                    $connection->query("update channel set uninvited='".$uninvited."' where channel_name='".$row['channel_name']."'");
+                  }
+                if($row['channel_name']=='general'){
+                    $joined=$row['joined']. "," . $username;
+                    $connection->query("update channel set joined='".$joined."' where channel_name='".$row['channel_name']."'");
+                }
+                if($row['channel_name']=='random'){
+                    $joined=$row['joined']. "," . $username;
+                    $connection->query("update channel set joined='".$joined."' where channel_name='".$row['channel_name']."'");
+                }
         }
+      }
     }
 }
 if ($_SESSION['sess_user'] != 'admin') {
